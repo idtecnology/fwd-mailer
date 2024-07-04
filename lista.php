ec96533ed09c433a35420a26f4458fbe4fc5fb79
Jesus Hernandez <jehfebles@gmail.com>
2024-07-04 17:00:26 -0400

    Release: 1.0.1 | Feature: Ajuste de la pool

--- lista.php
+++ lista.php
@@ -1,24 +1,3 @@
 <?php
 // POOL DE CORREOS
-return $forwardTo = [
-    0 => 'danny.valencia@idtecnology.com',
-    1 => 'jehfebles@gmail.com',
-    2 => 'danny.valencia@idtecnology.com',
-    3 => 'jehfebles@gmail.com',
-    4 => 'danny.valencia@idtecnology.com',
-    5 => 'danny.valencia@idtecnology.com',
-    6 => 'jehfebles@gmail.com',
-    7 => 'danny.valencia@idtecnology.com',
-    8 => 'jehfebles@gmail.com',
-    9 => 'danny.valencia@idtecnology.com',
-    10 => 'danny.valencia@idtecnology.com',
-    11 => 'jehfebles@gmail.com',
-    12 => 'danny.valencia@idtecnology.com',
-    13 => 'jehfebles@gmail.com',
-    14 => 'danny.valencia@idtecnology.com',
-    15 => 'danny.valencia@idtecnology.com',
-    16 => 'jehfebles@gmail.com',
-    17 => 'danny.valencia@idtecnology.com',
-    18 => 'jehfebles@gmail.com',
-    19 => 'danny.valencia@idtecnology.com',
-];
+return $forwardTo = [];
